ee76b0e6102f568018ed80b00719cf5f7e19ce2f
Omar Minaya <omiinaya@gmail.com>
2024-11-01 15:47:16 -0400

    .

--- mycog.py
+++ mycog.py
@@ -12,10 +12,12 @@ class MyCog(commands.Cog):
         # Your code will go here
         await ctx.send("I can do stuff!")
 
+    @commands.command()
     async def ping(self, ctx):
         """This pongs when you ping"""
         await ctx.send("Pong!")
 
+    @commands.command()
     async def rain(self, ctx):
         """This shows when it will rain next"""
         await ctx.send("Soon!")
