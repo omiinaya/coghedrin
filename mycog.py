eca00916f4c434e7362ec3ab4a86a00cfd471f14
root <root@discord-bot.mrx.local>
2024-11-01 19:36:08 +0000

    working example.

--- mycog.py
+++ mycog.py
@@ -1,13 +1,13 @@
 from redbot.core import commands
 
 class MyCog(commands.Cog):
-    """My ping pong cog"""
+    """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def ping(self, ctx):
+    async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("pong")
+        await ctx.send("I can do stuff!")
