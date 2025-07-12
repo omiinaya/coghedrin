4d790566f7bd7e10346f8532b19d543ee564a7fd
Omar Minaya <omiinaya@gmail.com>
2025-07-12 02:54:00 -0400

    .

--- localization.py
+++ localization.py
@@ -4,11 +4,15 @@ LOCALES = {
         'pong': 'Pong!',
         'roll': '{user}, you rolled a {number}',
         'invalid_condition': "Invalid condition. Please choose from 'day', 'night', 'normal', or 'rain'.",
+        'balding_percent': '{user} is {percent}% balding. o7',
+        'balding_none': "Congratz! You're not balding.",
     },
     'es': {
         'pong': '¡Pong!',
         'roll': '{user}, sacaste un {number}',
         'invalid_condition': "Condición inválida. Elija entre 'día', 'noche', 'normal' o 'lluvia'.",
+        'balding_percent': '{user} tiene {percent}% de calvicie. o7',
+        'balding_none': "¡Felicidades! No tienes calvicie.",
     }
 }
 
